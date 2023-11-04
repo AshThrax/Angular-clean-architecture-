c0d1927f1ee52ffc2f3939b585c3a94289563312
AshThrax <78923290+AshThrax@users.noreply.github.com>
2023-11-04 13:48:57 +0100

    implementation du gnx-player dans angular

--- fron-end/src/app/Component/music-player/music-player.component.ts
+++ fron-end/src/app/Component/music-player/music-player.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Track } from 'ngx-audio-player';
 @Component({
   selector: 'app-music-player',
   templateUrl: './music-player.component.html',
@@ -7,4 +7,73 @@ import { Component } from '@angular/core';
 })
 export class MusicPlayerComponent {
 
+onTrackPlaying(event:any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
 }
+
+
+onTrackPaused(event:any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
+}
+
+onEnded(event:any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
+}
+
+onNextTrackRequested(event:any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
+}
+
+
+onPreviousTrackRequested(event: any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
+}
+
+onTrackSelected(event:any) {
+    console.log(event);
+    // your logic which needs to
+    // be triggered once the
+    // track ends goes here.
+}
+  mssapDisplayTitle = true;
+  mssapDisablePositionSlider = true;
+  mssapDisplayRepeatControls = true;
+  mssapDisplayVolumeControls = true;
+  mssapDisplayVolumeSlider = false;
+
+
+    mssapPlaylist: Track[] = [
+    {
+      title: 'Audio Title',
+      link: 'Link to Streaming URL',
+      mediaType: 'stream'
+      },
+      {
+      title: 'Audio Title',
+      link: 'Link to Streaming URL',
+      mediaType: 'stream'
+      },
+      {
+      title: 'Audio Title',
+      link: 'Link to Streaming URL',
+      mediaType: 'stream'
+    }
+    ];
+
+}
+
